f1e13bd70115545048fa0870a5a5b036556e4be2
lazardjurovic <lazar.lax4525@gmail.com>
2016-09-30 17:48:28 +0200

    ALU update

--- ALU.vhd
+++ ALU.vhd
@@ -19,6 +19,8 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -30,10 +32,11 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity ALU is
-    Port ( alu_input_1 : in  STD_LOGIC_VECTOR (3 downto 0);
-           alu_input_2 : in  STD_LOGIC_VECTOR (3 downto 0);
-           alu_output : out  STD_LOGIC_VECTOR (3 downto 0);
-           command : in  STD_LOGIC_VECTOR (3 downto 0);
+    Port ( alu_input_1 : in  STD_LOGIC_VECTOR (3 downto 0); -- first ALU input ( 4 bit)
+           alu_input_2 : in STD_LOGIC_VECTOR (3 downto 0); -- second ALU input ( 4 bit)
+           alu_output : out  STD_LOGIC_VECTOR (3 downto 0); -- output signal of ALU ( 4 bit)
+           command : in STD_LOGIC_VECTOR (3 downto 0); -- one bit signal that tells us if ALU is busy or not
+			  busy : inout STD_LOGIC_VECTOR (0 downto 0); -- clock input
 			  clk : in STD_LOGIC);
 end ALU;
 
@@ -44,43 +47,45 @@ begin
 CALCULATE_RESULT : process(clk)
 begin
 
-if clk = "1" then
-		if command = "0001" then -- sabiranje ulaznih podataka
-			alu_output <= std_logic_vector(unsigned(alu_input_1) + unsigned(alu_input_2));
-		elsif command = "0010" then 
-			alu_output <= std_logic_vector(unsigned(alu_input_1) - unsigned(alu_input_2)); -- oduzimanje podataka 
-		elsif command = "0011" then --- mnozenje pomocu sabiranja
-		
-			I:= 0;
-			result := 0;
-			while I < unsigned(alu_input_1)  loop
-				
-				result:= result + unsigned(alu_input_2);
-				
-			end loop;
-			
-			alu_output <= result;
-			
-		elsif	command = "0100" then -- deljenje pomocu oduzimanja
-		
-			I:= 0;
-			result := alu_input_1;
-			while I < unsigned(alu_input_1)  loop
-				
-				result:= result - unsigned(alu_input_2);
-				
-			end loop;
-			
-			alu_output <= result;
-			
-		elsif command = "0101" then -- invertuje signal alu_input_1
-			
-			alu_output <= -alu_input_1;
+if rising_edge(clk) then
+	
+
+		if command = "0001" then -- addition of input 
+			busy <= "1";
+			alu_output <= alu_input_1 + alu_input_2;
+			busy <= "0";
+		elsif command = "0010" then
+			busy <= "1";
+			alu_output <= alu_input_1 - alu_input_2; -- substraction of input signals
+			busy <= "0";
+		elsif command = "0011" then
+			busy <= "1";
+			alu_output <= alu_input_1 and alu_input_2; -- AND operation
+			busy <= "0";
+		elsif command = "0100" then
+			busy <= "1";
+			alu_output <= alu_input_1 or alu_input_2; -- OR operation
+			busy <= "0";
+		elsif command = "0101" then
+			busy <= "1";
+			alu_output <= alu_input_1 xor alu_input_2; -- XOR operation
+			busy <= "0";
+		elsif command = "0110" then
+			busy <= "1";
+			alu_output <= not alu_input_1; -- NOT operation
+			busy <= "0";
+		elsif command = "0111" then --shift right
+			busy <= "1";
+			alu_output <= alu_input_1 - alu_input_1;
+			busy <= "0";
+		elsif command = "1000" then --shift left
+			busy <= "1";
+			alu_output <= alu_input_1 + alu_input_1;
+			busy <= "0";
 		
 		end if; 
 
 end if;
-
 end process;
 
 end Behavioral;
